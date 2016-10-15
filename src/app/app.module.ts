5b869347b2656cd7cef0af7acf199a11fce8ec84
Achim Kramer <achim@zibra.de>
2016-10-16 00:31:17 +0200

    Unnötige Page2 rausgeworfen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from "@angular/core";
 import {IonicApp, IonicModule} from "ionic-angular";
 import {StuttgartFinderApp} from "./app.component";
 import {Page1} from "../pages/page1/page1";
-import {Page2} from "../pages/page2/page2";
 import {ValuesPipe} from "../pipes/values";
 import {StuttgartMapsData} from "../providers/stuttgart-maps-data";
 import {StuttgartMapsCalculator} from "../services/stuttgart-maps-calculator";
@@ -11,7 +10,6 @@ import {StuttgartMapsCalculator} from "../services/stuttgart-maps-calculator";
   declarations: [
     StuttgartFinderApp,
     Page1,
-    Page2,
     ValuesPipe
   ],
   imports: [
@@ -20,8 +18,7 @@ import {StuttgartMapsCalculator} from "../services/stuttgart-maps-calculator";
   bootstrap: [IonicApp],
   entryComponents: [
     StuttgartFinderApp,
-    Page1,
-    Page2
+    Page1
   ],
   providers: [StuttgartMapsData, StuttgartMapsCalculator]
 })
