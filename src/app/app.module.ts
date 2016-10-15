e1705a214a599954d5038bca90aed4e7019e2055
Achim Kramer <achim@zibra.de>
2016-10-15 19:08:32 +0200

    Zwei eigene Pipes eingebaut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { StuttgartFinderApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
-
-import { StuttgartMapsData } from '../providers/stuttgart-maps-data';
-import { StuttgartMapsCoordinatesCalculator } from '../services/stuttgart-maps-coordinates-calculator';
+import {NgModule} from "@angular/core";
+import {IonicApp, IonicModule} from "ionic-angular";
+import {StuttgartFinderApp} from "./app.component";
+import {Page1} from "../pages/page1/page1";
+import {Page2} from "../pages/page2/page2";
+import {ValuesPipe} from "../pipes/values";
+import {CompletePipe} from "../pipes/complete";
+import {StuttgartMapsData} from "../providers/stuttgart-maps-data";
+import {StuttgartMapsCoordinatesCalculator} from "../services/stuttgart-maps-coordinates-calculator";
 
 @NgModule({
   declarations: [
     StuttgartFinderApp,
     Page1,
-    Page2
+    Page2,
+    ValuesPipe,
+    CompletePipe
   ],
   imports: [
     IonicModule.forRoot(StuttgartFinderApp)
